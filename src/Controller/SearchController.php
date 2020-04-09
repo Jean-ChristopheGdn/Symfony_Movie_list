bfc5721d288c006ae3884f6b4f494fad8b05f718
Blax Fontaine <valerie.gehle@epitech.eu>
2020-04-09 10:28:13 +0200

    search movies with keyword

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -15,28 +15,23 @@ class SearchController extends AbstractController
      */
     public function index()
     {
+
+      $titles = [];
+
       $client = HttpClient::create();
 
-      $response = $client->request('GET', 'https://api.themoviedb.org/3/search/movie?api_key=279e70c73121b427dcf04bc30ac9382f&language=en-US&page=1&include_adult=false&query=weed');
-      // var_dump($response);
-      $statusCode = $response->getStatusCode();
-
-      $contentType = $response->getHeaders()['content-type'][0];
-      // var_dump($contentType);
-      $content = $response->getContent();
+      $response = $client->request('GET', 'https://api.themoviedb.org/3/search/movie?api_key=279e70c73121b427dcf04bc30ac9382f&language=en-US&page=1&include_adult=false&query=' . $_POST["keyword"]);
 
       $content = $response->toArray();
-      // var_dump($content);
 
       $movies = $content['results'];
-      $titles = [];
       foreach($movies as $movie) {
         array_push($titles, $movie['title']);
       }
-      // var_dump($titles);
-        return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
-            'titles' => $titles
-        ]);
+
+      return $this->render('search/index.html.twig', [
+          'controller_name' => 'SearchController',
+          'titles' => $titles
+      ]);
     }
 }
